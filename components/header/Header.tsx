416d7b698d463e1b5a3589593d32061c507ccca4
lukasrzavsky <36069536+lukasrzavsky@users.noreply.github.com>
2025-05-17 21:24:22 +0200

    simple fade in animation added for header logo

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,14 +1,42 @@
 import { HEADER_HEIGHT } from '@/constants/common';
 import { Image } from 'expo-image';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
 
-export const Header = () => (
-  <View className={`w-full h-[${HEADER_HEIGHT}px] self-center`}>
-    <Image
-      source={require('../../assets/images/logo.png')}
-      style={{ width: '100%', height: '100%' }}
-      contentFit='contain'
-      transition={300}
-    />
-  </View>
-);
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+
+export const Header = () => {
+  const opacity = useSharedValue(0);
+
+  useEffect(() => {
+    opacity.value = withTiming(1, {
+      duration: 800,
+      easing: Easing.inOut(Easing.ease)
+    });
+  }, [opacity]);
+
+  const animatedImageStyle = useAnimatedStyle(() => {
+    return {
+      width: '100%',
+      height: '100%',
+      opacity: opacity.value
+    };
+  });
+
+  return (
+    <View className='w-full self-center' style={{ height: HEADER_HEIGHT }}>
+      <AnimatedImage
+        source={require('../../assets/images/logo.png')}
+        style={animatedImageStyle}
+        contentFit='contain'
+        transition={300}
+      />
+    </View>
+  );
+};
