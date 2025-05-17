eeded599396eb230177f8725e8dc12b1f21bbad5
lukasrzavsky <36069536+lukasrzavsky@users.noreply.github.com>
2025-05-17 20:28:55 +0200

    screen view adjustments

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,33 @@
-import { Text, View } from 'react-native';
+import { Layout } from '@/components/layout/Layout';
+import { PersonItem } from '@/components/personItem/PersonItem';
+import { useGetPeople } from '@/hooks/useGetPeople';
+import { FlashList } from '@shopify/flash-list';
+import { useMemo } from 'react';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Text className='text-red-400'>
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+const LIST_ITEM_SIZE = 386;
+
+const Index = () => {
+  const { data } = useGetPeople();
+
+  const people = useMemo(
+    () => data?.pages?.flatMap(({ results }) => results ?? []) ?? [],
+    [data?.pages]
   );
-}
+
+  return (
+    <Layout>
+      <FlashList
+        data={people}
+        renderItem={({ item, index }) => (
+          <PersonItem detail={item} id={index + 1} />
+        )}
+        keyExtractor={item => item.name}
+        showsVerticalScrollIndicator={false}
+        estimatedItemSize={LIST_ITEM_SIZE}
+        contentContainerClassName='pb-[30px]'
+      />
+    </Layout>
+  );
+};
+
+export default Index;
