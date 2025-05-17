22cd0afc8f667f0de9f2a8705136619ece993165
lukasrzavsky <36069536+lukasrzavsky@users.noreply.github.com>
2025-05-17 19:54:29 +0200

    SafeAreaView with StatusBar added to _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
+import { Header } from '@/components/header/Header';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import '../global.css';
 
 if (__DEV__) {
@@ -11,8 +14,18 @@ export default function RootLayout() {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack />
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaView className='flex-1 bg-black'>
+          <Stack
+            screenOptions={{
+              headerTransparent: true,
+              headerTitle: () => <Header />
+            }}
+          />
+        </SafeAreaView>
+      </QueryClientProvider>
+      <StatusBar barStyle='light-content' />
+    </>
   );
 }
